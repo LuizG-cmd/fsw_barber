78382b86e2a294db6722641c1a2ca0a31365c7a8
LG <guilhermeohtek68@gmail.com>
2024-08-13 11:30:49 -0300

    feat: add booking item

--- fsw-barber/src/app/page.tsx
+++ fsw-barber/src/app/page.tsx
@@ -1,22 +1,37 @@
 "use client"
 
+/*COMPONENTES SHADCN*/ 
 import { Button } from "./_components/ui/button"
-import Header from "./_components/header"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
+import { Badge } from "./_components/ui/badge"
+import { Card, CardContent } from "./_components/ui/card"
 import { Input } from "./_components/ui/input"
+
+/* MEUS COMPONENTS */
+import Header from "./_components/header"
+
 import { SearchIcon } from "lucide-react"
 
 import Image from "next/image"
 
+
+
 const Home = () => {
   return (
     <div>
+
+      {/* HEADER */}
       <Header />
+      {/* HEADER */}
 
 
       <div className="p-5">
+        {/* TEXTO */}
         <h2 className="text-xl font-bold">Olá, Luiz</h2>
         <p>Segunda-feira, 12 de Agosto</p>
 
+
+        {/* BUSCA */}
         <div className="flex items-center gap-2 mt-6">
           <Input placeholder="Faça sua busca..." />
           <Button>
@@ -24,6 +39,7 @@ const Home = () => {
           </Button>
         </div>
 
+        {/* IMAGEM */}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             alt="Agende nos melhores com FSW Barber"
@@ -32,10 +48,38 @@ const Home = () => {
             className="rounded-xl object-cover"
           />
         </div>
-        
-      </div>
 
-      
+        {/* AGENDAMENTO */}
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+          {/* ESQUERDA */}
+          <div className="flex flex-col gap-5 py-5 pl-5">
+            <Badge className="w-fit">
+              Confirmado
+            </Badge>
+            <h3>
+              Corte de Cabelo
+            </h3>
+
+            <div className="flex items-center gap-2">
+            <Avatar className="h-6 w-6">
+              <AvatarImage src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.peterbe.com%2Fplog%2Frandom-avatars-in-django-python&psig=AOvVaw2BmQgstmUOVG7g2QerjaGG&ust=1723644864532000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPiyv8-T8ocDFQAAAAAdAAAAABAU">
+              </AvatarImage>
+            </Avatar>
+            <p>
+                Barbearia L.G
+            </p>
+            </div>
+          </div>
+
+          <div className="flex flex-col items-center justify-center px-5 border-l-2 border-solid">
+            <p className="text-sm">Agosto</p>
+            <p className="text-xl">18</p>
+            <p className="text-sm">19:15</p>
+          </div>
+          </CardContent>
+        </Card>
+      </div> 
     </div>
   )
 }
